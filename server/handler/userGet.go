b9f693d2175366736dfedfbd3b24710411398db6
Francis Dolan <francis.dolan@gmail.com>
2024-09-28 14:27:32 +0100

    Updated UserGet

--- server/handler/userGet.go
+++ server/handler/userGet.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 type User struct {
@@ -14,8 +18,16 @@ type User struct {
 func UserGet(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("In Service 1 UserGet")
 
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if nil != err {
+		log.WithError(err).Error("cannot retrieve URL ID paramater")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(User{ID: 1})
+
+	err = json.NewEncoder(w).Encode(User{ID: id, Email: fmt.Sprintf("example_%d@example.com", id)})
 	if nil != err {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
